a40cbbcb76c98093663d0b4b30f40fd03efc7023
abdulmughnialfikri <dev.abdulmughni@gmail.com>
2021-03-08 13:45:32 +0700

    Add resource

---
+++ app/src/main/java/com/abdulmughni/personal/thefortnightly/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.abdulmughni.personal.thefortnightly
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.RequiresApi
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    private val loadingTime = 3000
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+        Glide.with(this)
+            .load(R.drawable.fortnightly)
+            .into(fortnightly_img)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            finish()
+        }, loadingTime.toLong())
+    }
+}
